7b3286702498738c666adf212e30a8c9b5c27d1b
Bharat097 <bharatvaghela097@gmail.com>
2020-06-07 23:48:05 +0530

    Added file

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import Table from './Table'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h1>Hello, React!</h1>
+        <Table />
+        <Table />
+      </div>
+    )
+  }
+}
+
+export default App
